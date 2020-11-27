71210087b925ea8af4a90f64005351524e6bfc18
Rayfalling <wanghaiwe.i.1999@163.com>
2020-11-27 23:00:02 +0800

    add data cache model

---
+++ SpaceTree/Libs/Cache/DirectoryCache.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SpaceTree.Model;
+
+namespace SpaceTree.Libs.Model {
+    /// <summary>
+    /// 目录信息缓存
+    /// </summary>
+    internal class DirectoryCache {
+        /// <summary>
+        /// 文件夹路径
+        /// </summary>
+        public string Uri { get; set; }
+
+        /// <summary>
+        /// 文件夹大小
+        /// </summary>
+        public long Length { get; set; }
+
+        /// <summary>
+        /// 上次统计日期
+        /// </summary>
+        public DateTime LastSeen { get; set; }
+
+        /// <summary>
+        /// 当前目录文件信息
+        /// </summary>
+        public List<FileCache> FileCaches;
+
+        /// <summary>
+        /// 子目录信息
+        /// </summary>
+        public List<DirectoryCache> SubDirectoryCaches;
+
+        public DirectoryCache(string uri, long length, DateTime lastSeen) {
+            Uri = uri;
+            Length = length;
+            LastSeen = lastSeen;
+            FileCaches = new List<FileCache>();
+            SubDirectoryCaches = new List<DirectoryCache>();
+        }
+
+        public DirectoryCache() {
+            Uri = "";
+            Length = 0;
+            LastSeen = DateTime.Now;
+            FileCaches = new List<FileCache>();
+            SubDirectoryCaches = new List<DirectoryCache>();
+        }
+
+        /// <summary>
+        /// 获取原始大小
+        /// </summary>
+        /// <returns></returns>
+        public long GetRawSize() {
+            CheckSize();
+            return Length;
+        }
+
+        /// <summary>
+        /// 获取格式化大小
+        /// </summary>
+        /// <returns></returns>
+        public string GetFormatSize() {
+            CheckSize();
+            return SizeUtils.GetPrettySize(Length);
+        }
+
+        private void CheckSize() {
+            if (Length != 0 || (SubDirectoryCaches.Count == 0 && FileCaches.Count == 0)) return;
+            long currentSize = 0;
+            Parallel.ForEach(FileCaches, fileCache => Interlocked.Add(ref currentSize, fileCache.Length));
+            Parallel.ForEach(SubDirectoryCaches,
+                directoryCaches => Interlocked.Add(ref currentSize, directoryCaches.GetRawSize()));
+            Length = currentSize;
+        }
+    }
+}
---
+++ SpaceTree/Libs/Cache/FileCache.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SpaceTree.Model {
+    internal class FileCache {
+        /// <summary>
+        /// 文件路径
+        /// </summary>
+        public string Uri { get; set; }
+
+        /// <summary>
+        /// 文件大小
+        /// </summary>
+        public long Length { get; set; }
+
+        /// <summary>
+        /// 上次统计日期
+        /// </summary>
+        public DateTime LastSeen { get; set; }
+
+        public FileCache(string uri, long length, DateTime lastSeen) {
+            Uri = uri;
+            Length = length;
+            LastSeen = lastSeen;
+        }
+    }
+}
