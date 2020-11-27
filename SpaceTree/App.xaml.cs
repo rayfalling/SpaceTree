4787f660a1c1bfa875d8858b1b4104f146a2126c
Rayfalling <wanghaiwe.i.1999@163.com>
2020-11-27 15:21:15 +0800

    Init project

---
+++ SpaceTree/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace SpaceTree {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+        private void StartUp(object sender, StartupEventArgs e) {
+            //TODO Load config & cache here
+
+            // Create main application window, starting minimized if specified
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+        }
+    }
+}
