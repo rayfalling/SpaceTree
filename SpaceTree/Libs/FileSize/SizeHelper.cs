2a9b2a14b2a05779fb7663f78914fa5ac2e3d02f
Rayfalling <wanghaiwe.i.1999@163.com>
2020-11-27 22:59:32 +0800

    add size helper

---
+++ SpaceTree/Libs/FileSize/SizeHelper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SpaceTree.Libs.Model {
+    internal static class SizeHelper {
+        private const int Rate = 1024;
+
+        /// <summary>
+        /// 获取格式化大小
+        /// </summary>
+        /// <param name="size">文件/文件夹大小</param>
+        /// <returns></returns>
+        public static string GetPrettySize(long size) { return GetPrettySize(size, GetProperSizeLevel(size)); }
+
+        /// <summary>
+        /// 获取格式化大小
+        /// </summary>
+        /// <param name="size">文件/文件夹大小</param>
+        /// <param name="sizeLevel">大小级别</param>
+        /// <returns></returns>
+        public static string GetPrettySize(long size, SizeLevel sizeLevel) {
+            var rate = (int) sizeLevel;
+            while (rate != 0) {
+                size /= Rate;
+                rate--;
+            }
+
+            return $"{size} {PrettySizeLevel(sizeLevel)}";
+        }
+
+        /// <summary>
+        /// 获取合适进制
+        /// </summary>
+        /// <param name="size"></param>
+        /// <returns></returns>
+        public static SizeLevel GetProperSizeLevel(long size) {
+            var rate = 0;
+            while (size / (double) Rate > Rate * 0.8) {
+                size /= Rate;
+                rate++;
+                if (size >= 6)
+                    break;
+            }
+
+            return (SizeLevel) rate;
+        }
+
+        /// <summary>
+        /// 翻译文件进制英文
+        /// </summary>
+        /// <param name="sizeLevel"></param>
+        /// <returns></returns>
+        private static string PrettySizeLevel(SizeLevel sizeLevel) {
+            CheckSizeLevel(sizeLevel);
+            return sizeLevel switch {
+                SizeLevel.Byte => "B",
+                SizeLevel.Kilobyte => "KB",
+                SizeLevel.Megabyte => "MB",
+                SizeLevel.Gigabyte => "GB",
+                SizeLevel.Terabyte => "TB",
+                SizeLevel.Petabyte => "PB",
+                SizeLevel.Exabyte => "EB",
+                _ => throw new ArgumentOutOfRangeException(nameof(sizeLevel), sizeLevel, null)
+            };
+        }
+
+        /// <summary>
+        /// 检查枚举类是否超界
+        /// </summary>
+        /// <param name="sizeLevel"></param>
+        private static void CheckSizeLevel(SizeLevel sizeLevel) {
+            if (sizeLevel > SizeLevel.Exabyte)
+                Logger.GetInstance().Log(LogLevel.Error, "Incorrect SizeLevel");
+        }
+    }
+}
---
+++ SpaceTree/Libs/FileSize/SizeLevel.cs
@@ -0,0 +1,11 @@
+﻿namespace SpaceTree.Libs.Model {
+    internal enum SizeLevel {
+        Byte = 0,
+        Kilobyte = 1,
+        Megabyte = 2,
+        Gigabyte = 3,
+        Terabyte = 4,
+        Petabyte = 5,
+        Exabyte = 6
+    }
+}
